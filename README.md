08829b30954fe0fbc4d28e203920410ce82c9b0e
user1 <user1@user1s-MBP.lan>
2017-08-17 22:21:28 +1000

    Back to setState

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## Keep the keyboard up during `onClick` events
+
+*`onBlur` causing issues on Apple based browsers
+* Needs setTimeout doesn't work properly on mobile, so have to do a bogus setState for now
--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
   componentWillMount() {
     setTimeout(() => {
-  this.focus()
-}, 0);
+      this.focus();
+    }, 0);
   }
   focus = () => {
     this.refs.searcher.focus();
-  }
+  };
   test = () => {
     this.refs.searcher.focus();
-  }
+  };
   render() {
     return (
       <div>
-      <button onClick={this.test}> click</button>
-        <input onBlur={this.focus} ref="searcher" placeholder="Search" type="text" />
-        </div>
+        <button onClick={this.test}> click</button>
+        <input
+          onBlur={this.focus}
+          ref="searcher"
+          placeholder="Search"
+          type="text"
+        />
+      </div>
     );
   }
 }
